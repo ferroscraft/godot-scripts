1d9126383bcbed61d71aef742a1a67bd65a12bd6
ferroscraft <ferroscraft@gmail.com>
2021-05-17 06:46:36 -0300

    adicao de controle de sensibilidade do mouse

--- first_person_character_controller.gd
+++ first_person_character_controller.gd
@@ -3,13 +3,15 @@ extends KinematicBody
 var move: Vector3
 var move_axis: Vector2
 
-export var gravity: int = 10
-export var jump: int = 10
+onready var camera: Camera = get_node("Camera")
+
+export var gravity: int = 15
 export var speed: int = 10
+export var jump: int = 10
+export var mouse_sensitivity: float = 0.5
 
 
 func _input(event: InputEvent) -> void:
-	
 	move_axis.y = Input.get_action_strength("game_up") - Input.get_action_strength("game_down")
 	move_axis.x = Input.get_action_strength("game_left") - Input.get_action_strength("game_right")
 	move_axis = move_axis.normalized()
@@ -18,19 +20,17 @@ func _input(event: InputEvent) -> void:
 		move.y = Input.get_action_strength("game_jump") * jump
 	
 	if event is InputEventMouseMotion:
-		rotate_y(deg2rad(event.relative.x*-1))
-		$Camera.rotate_x(deg2rad(event.relative.y*-1))
-		$Camera.rotation_degrees.x = clamp($Camera.rotation_degrees.x, -90, 90)
+		rotate_y(deg2rad(event.relative.x*-mouse_sensitivity))
+		camera.rotate_x(deg2rad(event.relative.y*-mouse_sensitivity))
+		camera.rotation_degrees.x = clamp(camera.rotation_degrees.x, -90, 90)
 	
 func _physics_process(delta: float) -> void:
+	var z = global_transform.basis.z
+	var x = global_transform.basis.x
 	
-	var forward = global_transform.basis.z
-	var right = global_transform.basis.x
-	
-	move.z = -(forward * move_axis.y + right * move_axis.x).z * speed
-	move.x = -(forward * move_axis.y + right * move_axis.x).x * speed
+	move.z = -(z * move_axis.y + x * move_axis.x).z * speed
+	move.x = -(z * move_axis.y + x * move_axis.x).x * speed
 	
 	move.y -= gravity*delta
 	
 	move = move_and_slide(move, Vector3.UP)
-	
